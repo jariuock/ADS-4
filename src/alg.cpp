a7f99c3c102b1c88902d173e2393a5993d144d90
jariuock <garanin.019@gmail.com>
2025-04-06 14:21:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -33,7 +33,8 @@ int binarySearchCount(int* arr, int left, int right, int target) {
         if (arr[mid] == target) {
             first = mid;
             r = mid - 1;
-        } else if (arr[mid] < target)
+        }
+        else if (arr[mid] < target)
             l = mid + 1;
         else
             r = mid - 1;
@@ -48,7 +49,8 @@ int binarySearchCount(int* arr, int left, int right, int target) {
         if (arr[mid] == target) {
             last = mid;
             l = mid + 1;
-        } else
+        }
+        else
             r = mid - 1;
     }
     return last - first + 1;
